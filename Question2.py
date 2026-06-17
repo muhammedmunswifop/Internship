4a2db7659233b68b9cad3c74177f3e32f6143992
Muhammed Munswif OP <muhammedmunswifop@gmail.com>
2026-06-17 13:28:27 +0530

    Day01

---
+++ Question2.py
@@ -0,0 +1,8 @@
+print("2.List 5 features of Python")
+print("""
+      1.Easy to learn and use
+      2.Interpreted language
+      3.Object-oriented
+      4.Cross-platform
+      5.Large standard library
+""")
